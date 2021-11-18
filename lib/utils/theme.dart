65bdfed3d08e5af3a34cbd4264ee0432d02162b0
154_5WH <isamentot@gmail.com>
2021-11-18 08:55:24 +0700

    penambahan method birunyacinta

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -1,5 +1,24 @@
 import 'package:flutter/material.dart';
 
+ThemeData birunyacinta() {
+  return ThemeData(
+      primarySwatch: const MaterialColor(
+    (0xFF104B97),
+    <int, Color>{
+      50: Color(0xFF104B97),
+      100: Color(0xFF104B97),
+      200: Color(0xFF104B97),
+      300: Color(0xFF104B97),
+      400: Color(0xFF104B97),
+      500: Color(0xFF104B97),
+      600: Color(0xFF104B97),
+      700: Color(0xFF104B97),
+      800: Color(0xFF104B97),
+      900: Color(0xFF104B97),
+    },
+  ));
+}
+
 AppBar widgetAppBar() {
   return AppBar(
     elevation: 0,
