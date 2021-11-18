31fd07e4a6d03cd99174ab0175007562d0d61397
154_5WH <isamentot@gmail.com>
2021-11-18 15:38:28 +0700

    full update notice page (done)

--- lib/page/notice.dart
+++ lib/page/notice.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_magang2/page/masuk.dart';
 import 'package:flutter_magang2/utils/theme.dart';
 
 class Notice extends StatelessWidget {
@@ -9,19 +10,167 @@ class Notice extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.amber,
       appBar: widgetAppBar(),
-      body: Container(
-        child: Padding(
-          padding: const EdgeInsets.only(
-            top: 74,
-            right: 75,
-            left: 80,
+      body: Column(
+        children: [
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: 74,
+                right: 75,
+                left: 80,
+              ),
+              child: Image.asset(
+                "assets/notice.png",
+                height: 228,
+                width: 228,
+              ),
+            ),
+            padding: const EdgeInsets.only(),
           ),
-          child: Image.asset(
-            "assets/notice.png",
-            height: 228,
-            width: 228,
+          const Center(
+            child: Padding(
+              padding: EdgeInsets.only(
+                top: 30,
+                bottom: 10,
+              ),
+              child: Text(
+                "Device Berbeda !",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.red),
+              ),
+            ),
           ),
-        ),
+          const Text(
+            "Device regristasi dan login berbeda!",
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w300,
+              color: Colors.black,
+            ),
+          ),
+          const Text(
+            "Hati-hati penyalah gunaan akun",
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w300,
+              color: Colors.black,
+            ),
+          ),
+          const Text(
+            "Jangan sebarkan passsword Anda",
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w300,
+              color: Colors.black,
+            ),
+          ),
+          const Text(
+            "kepada orang lain",
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w300,
+              color: Colors.black,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(
+              top: 150,
+            ),
+            child: Text(
+              "Yakin ganti device ?",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w300,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 15,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                SizedBox(
+                  height: 48,
+                  width: 128,
+                  child: ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const masukpak()));
+                    },
+                    icon: const Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                    ),
+                    label: const Text(
+                      "Kembali",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                      side: const BorderSide(color: Colors.white, width: 3),
+                      borderRadius: BorderRadius.circular(17),
+                    )),
+                  ),
+                ),
+                SizedBox(
+                  height: 48,
+                  width: 128,
+                  child: ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.login_rounded,
+                      color: Colors.indigo,
+                    ),
+                    label: const Text(
+                      "Lanjut",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.indigo,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          side:
+                              const BorderSide(color: Colors.indigo, width: 3),
+                          borderRadius: BorderRadius.circular(17),
+                        )),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "Paket multi devices ?",
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+          ),
+          const Text(
+            "Lihat paket",
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w300,
+              color: Colors.indigo,
+            ),
+          )
+        ],
       ),
     );
   }
