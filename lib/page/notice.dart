65a612a0fc710da3eba229f7c5771b91d26ca1d2
154_5WH <isamentot@gmail.com>
2021-11-19 16:50:07 +0700

    update media query

--- lib/page/notice.dart
+++ lib/page/notice.dart
@@ -8,179 +8,198 @@ class Notice extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final mediaquerybody = MediaQuery.of(context).size.height;
+    final myappbar = widgetAppBar();
+    final bodyheight = mediaquerybody -
+        MediaQuery.of(context).padding.top -
+        myappbar.preferredSize.height;
+
     return Scaffold(
       backgroundColor: Colors.amber,
-      appBar: widgetAppBar(),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(
-              top: 74,
-              right: 75,
-              left: 80,
-            ),
-            child: Image.asset(
-              "assets/notice.png",
-              height: 228,
-              width: 228,
-            ),
-          ),
-          const Center(
-            child: Padding(
-              padding: EdgeInsets.only(
-                top: 30,
-                bottom: 10,
-              ),
-              child: Text(
-                "Device Berbeda !",
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.normal,
-                    color: Colors.red),
-              ),
-            ),
-          ),
-          const Text(
-            "Device regristasi dan login berbeda!",
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w300,
-              color: Colors.black,
-            ),
-          ),
-          const Text(
-            "Hati-hati penyalah gunaan akun",
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w300,
-              color: Colors.black,
-            ),
-          ),
-          const Text(
-            "Jangan sebarkan passsword Anda",
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w300,
-              color: Colors.black,
-            ),
-          ),
-          const Text(
-            "kepada orang lain",
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w300,
-              color: Colors.black,
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(
-              top: 150,
-            ),
-            child: Text(
-              "Yakin ganti device ?",
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w300,
-                color: Colors.black,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-              top: 15,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                SizedBox(
-                  height: 48,
-                  width: 128,
-                  child: ElevatedButton.icon(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const Masukpak()));
-                    },
-                    icon: const Icon(
-                      Icons.arrow_back,
-                      color: Colors.white,
-                    ),
-                    label: const Text(
-                      "Kembali",
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                      side: const BorderSide(color: Colors.white, width: 3),
-                      borderRadius: BorderRadius.circular(17),
-                    )),
+      appBar: myappbar,
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: bodyheight * 0.3,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 25,
+                  ),
+                  child: Image.asset(
+                    "assets/notice.png",
+                    height: 228,
+                    width: 228,
                   ),
                 ),
-                SizedBox(
-                  height: 48,
-                  width: 128,
-                  child: ElevatedButton.icon(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const Adapak()));
-                    },
-                    icon: const Icon(
-                      Icons.login_rounded,
-                      color: Colors.indigo,
+              ),
+            ),
+            SizedBox(
+              height: bodyheight * 0.7,
+              child: Column(
+                children: [
+                  const Center(
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                        top: 30,
+                        bottom: 10,
+                      ),
+                      child: Text(
+                        "Device Berbeda !",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.red),
+                      ),
                     ),
-                    label: const Text(
-                      "Lanjut",
+                  ),
+                  const Text(
+                    "Device regristasi dan login berbeda!",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const Text(
+                    "Hati-hati penyalah gunaan akun",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const Text(
+                    "Jangan sebarkan passsword Anda",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const Text(
+                    "kepada orang lain",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(
+                      top: 200,
+                    ),
+                    child: Text(
+                      "Yakin ganti device ?",
                       style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 15,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        SizedBox(
+                          height: 48,
+                          width: 128,
+                          child: ElevatedButton.icon(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const Masukpak()));
+                            },
+                            icon: const Icon(
+                              Icons.arrow_back,
+                              color: Colors.white,
+                            ),
+                            label: const Text(
+                              "Kembali",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                              side: const BorderSide(
+                                  color: Colors.white, width: 3),
+                              borderRadius: BorderRadius.circular(17),
+                            )),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 48,
+                          width: 128,
+                          child: ElevatedButton.icon(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const Adapak()));
+                            },
+                            icon: const Icon(
+                              Icons.login_rounded,
+                              color: Colors.indigo,
+                            ),
+                            label: const Text(
+                              "Lanjut",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.indigo,
+                              ),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                                primary: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                  side: const BorderSide(
+                                      color: Colors.indigo, width: 3),
+                                  borderRadius: BorderRadius.circular(17),
+                                )),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(
+                      top: 15,
+                    ),
+                    child: Text(
+                      "Paket multi devices ?",
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(
+                      top: 15,
+                    ),
+                    child: Text(
+                      "Lihat paket",
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w300,
                         color: Colors.indigo,
                       ),
                     ),
-                    style: ElevatedButton.styleFrom(
-                        primary: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          side:
-                              const BorderSide(color: Colors.indigo, width: 3),
-                          borderRadius: BorderRadius.circular(17),
-                        )),
                   ),
-                ),
-              ],
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(
-              top: 15,
-            ),
-            child: Text(
-              "Paket multi devices ?",
-              style: TextStyle(
-                fontSize: 12,
-                fontWeight: FontWeight.w300,
+                ],
               ),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(
-              top: 15,
-            ),
-            child: Text(
-              "Lihat paket",
-              style: TextStyle(
-                fontSize: 12,
-                fontWeight: FontWeight.w300,
-                color: Colors.indigo,
-              ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
